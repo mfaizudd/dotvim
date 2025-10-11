13cc97c0491c858f4092e46d966fcc1db4ad8f3d
Muhammad Faizud Daroin <m@faizud.net>
2025-10-11 20:38:01 +0700

    fix: custom lsp setup, disable esc terminal exit

--- init.lua
+++ init.lua
@@ -185,7 +185,7 @@ vim.keymap.set('n', '<leader>y', '<cmd>OSCYankVisual<cr>', { desc = 'Yank to sys
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -1024,6 +1024,7 @@ require('lazy').setup({
     },
   },
 })
+require 'custom.lsp'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
