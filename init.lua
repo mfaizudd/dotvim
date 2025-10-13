6b32136331b66f670917d84546f565a2622cf9f8
Muhammad Faizud Daroin <faizud.daroin@formulatrix.com>
2025-10-13 10:32:34 +0700

    feat: harpoon, guess indent, lsp

--- init.lua
+++ init.lua
@@ -257,7 +257,12 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  {
+    'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+    config = function()
+      require('guess-indent').setup {}
+    end,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -1025,6 +1030,7 @@ require('lazy').setup({
   },
 })
 require 'custom.lsp'
+require 'custom.harpoon_setup'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -30,4 +30,40 @@ return {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
+  {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+      -- Setup orgmode
+      require('orgmode').setup {
+        org_agenda_files = '~/orgfiles/**/*',
+        org_default_notes_file = '~/orgfiles/refile.org',
+      }
+
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  },
+  { 'dhruvasagar/vim-table-mode' },
+  { 'akinsho/org-bullets.nvim' },
+  {
+    'mrshmllow/orgmode-babel.nvim',
+    dependencies = {
+      'nvim-orgmode/orgmode',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    cmd = { 'OrgExecute', 'OrgTangle' },
+    opts = {
+      -- by default, none are enabled
+      langs = { 'python', 'lua', ... },
+
+      -- paths to emacs packages to additionally load
+      load_paths = {},
+    },
+  },
 }
