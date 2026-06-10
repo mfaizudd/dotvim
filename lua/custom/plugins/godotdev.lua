2cd47e2d2dda528687918082b92a021c6ad1efad
Muhammad Faizud Daroin <faizud.daroin@formulatrix.com>
2026-06-10 13:16:37 +0700

    feat: godot plugin

---
+++ lua/custom/plugins/godotdev.lua
@@ -0,0 +1,7 @@
+vim.pack.add({
+  "https://github.com/Mathijs-Bakker/godotdev.nvim",
+  "https://github.com/mfussenegger/nvim-dap",
+  "https://github.com/rcarriga/nvim-dap-ui",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+})
+require("godotdev").setup({})
