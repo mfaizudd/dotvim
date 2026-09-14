d9d5988b9259b4562fa513a9a62ab629100d8f66
Muhammad Faizud Daroin <faizud.daroin@formulatrix.com>
2026-09-14 12:20:04 +0700

    Revert "fix: remove roslyn config"
    This reverts commit 4232b9a180a9cfe18e6510b3d82d3a0f398367e6.

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -30,6 +30,66 @@ vim.diagnostic.config {
 }
 -- vim.lsp.enable { 'ts_ls', 'vue_ls' } -- If using `ts_ls` replace `vtsls` to `ts_ls`
 
+-- OMNISHARP
+local roslyn_config = {
+  settings = {
+    FormattingOptions = {
+      -- Enables support for reading code style, naming convention and analyzer
+      -- settings from .editorconfig.
+      EnableEditorConfigSupport = true,
+      -- Specifies whether 'using' directives should be grouped and sorted during
+      -- document formatting.
+      OrganizeImports = true,
+    },
+    MsBuild = {
+      -- If true, MSBuild project system will only load projects for files that
+      -- were opened in the editor. This setting is useful for big C# codebases
+      -- and allows for faster initialization of code navigation features only
+      -- for projects that are relevant to code that is being edited. With this
+      -- setting enabled OmniSharp may load fewer projects and may thus display
+      -- incomplete reference lists for symbols.
+      LoadProjectsOnDemand = false,
+      EnablePackageAutoRestore = true,
+    },
+    RoslynExtensionsOptions = {
+      -- Enables support for roslyn analyzers, code fixes and rulesets.
+      EnableAnalyzersSupport = true,
+      -- Enables support for showing unimported types and unimported extension
+      -- methods in completion lists. When committed, the appropriate using
+      -- directive will be added at the top of the current file. This option can
+      -- have a negative impact on initial completion responsiveness,
+      -- particularly for the first few completion sessions after opening a
+      -- solution.
+      EnableImportCompletion = true,
+      -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+      -- true
+      AnalyzeOpenDocumentsOnly = false,
+    },
+    Sdk = {
+      -- Specifies whether to include preview versions of the .NET SDK when
+      -- determining which version to use for project loading.
+      IncludePrereleases = false,
+    },
+  },
+}
+vim.lsp.config('omnisharp', roslyn_config)
+vim.lsp.config('roslyn', {
+  on_attach = function() print 'Roslyn attached' end,
+  settings = {
+    -- ['csharp|background_analysis'] = {
+    --   dotnet_analyzer_diagnostics_scope = 'fullSolution',
+    --   dotnet_compiler_diagnostics_scope = 'fullSolution',
+    -- },
+    ['csharp|inlay_hints'] = {
+      csharp_enable_inlay_hints_for_implicit_object_creation = true,
+      csharp_enable_inlay_hints_for_implicit_variable_types = true,
+    },
+    ['csharp|code_lens'] = {
+      dotnet_enable_references_code_lens = true,
+    },
+  },
+})
+
 -- OCaml
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.onTypeFormatting = nil
